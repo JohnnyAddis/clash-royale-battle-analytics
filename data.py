2e2e2aa0e8d4abd887a7c98cd25c88af62b2efd8
Johnny Addis <johnnyaddis23@gmail.com>
2025-12-25 14:55:41 -0600

    removing dummy file and making smallest possible change to support basic multi-user polling

--- data.py
+++ data.py
@@ -7,8 +7,8 @@ from config import PLAYER_TAG
 POLLING_INTERVAL_SECONDS = 5 #10 minutes for now
 
 
-def poll_once(conn):
-    battles = fetch_battle_log(PLAYER_TAG)
+def poll_once(conn, player_tag):
+    battles = fetch_battle_log(player_tag)
 
     inserted = 0
     for raw in battles:
@@ -67,11 +67,12 @@ def main():
     
     # ---- END TEMP ----
     try:
-        active_players = get_active_players(conn)
-        print(f"[debug] active players from DB: {active_players}")
         while True:
             try:
-                poll_once(conn)
+                active_players = get_active_players(conn)
+                print(f"[debug] active players from DB: {active_players}")
+                for player_tag in active_players:
+                    poll_once(conn, player_tag)
             except Exception as e:
                 print(f'[error] polling failed: {e}')
             print(f'[sleep] waiting {POLLING_INTERVAL_SECONDS} seconds\n')
--- practice.py
+++
@@ -1,5 +0,0 @@
-from db import get_active_players, get_connection
-
-
-conn = get_connection()
-print(get_active_players(conn))
